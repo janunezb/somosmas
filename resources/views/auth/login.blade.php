a649a40f931cb2b893bb6fff66e837d583fc02f0
Johan Morales <johan.morales@cellvoz.com.co>
2023-02-03 16:59:38 -0500

    ADD: Agregar visualización de contraseña en login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,10 +11,17 @@
     <form action="" class="mt-4" method="POST">
 
     @csrf
-    
-        <input type="text" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Número de cédula" id="cedula" name="documento">
-        
-        <input type="password" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Ingresa tu contraseña" id="password" name="password">
+    <div class="">
+        <div class="single-input-item">
+            <label for="cedula" >Usuario</label>
+            <input type="text" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Número de cédula" id="cedula" name="documento">
+        </div>
+        <br>
+        <div class="single-input-item">
+            <label for="cedula" >Contraseña</label>
+            <span class="icon-eye3"><i class="fa-solid fa-eye-slash"></i></span>
+            <input type="password" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Ingresa tu contraseña" id="password" name="password">
+        </div>
     
     @error('message')
     <p class="border border-red-500 rounded-md bg-red-100 w-full text-red-600 p-2 my-2">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <script src="https://kit.fontawesome.com/72557239af.js" crossorigin="anonymous"></script>
+    <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/splider-js.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/swiper_noti.css') }}">
 
 </head>
+
 <body class="bg-black">
     <nav class="flex py-5 bg-pink-500 text-white">
         <div class="w-1/2 px-12 mr-auto">
-            <p class="text-2xl font-bold">Somos Más<p>
+            <p class="text-2xl font-bold">Somos Más
+            <p>
         </div>
 
         <ul class="w-1/2 px-16 ml-auto flex justify-end pt-1">
             @if (auth()->check())
-
-
             @endif
         </ul>
 
@@ -27,4 +40,5 @@
 
 
 </body>
+
 </html>
