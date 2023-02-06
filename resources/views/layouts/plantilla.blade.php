0b5cae26c16d312b99c4e3b60c59125555aff158
Johan Morales <johan.morales@cellvoz.com.co>
2023-02-06 10:02:02 -0500

    Cambios contraseñas

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css')}}">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/72557239af.js" crossorigin="anonymous"></script>
     <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
@@ -80,7 +80,7 @@
                             
                 </div>
                 
-                <div class="pr-3">
+                <div class="pr-0">
                     @if (auth()->user()->foto)
                     <img src="../images/fotos/{{auth()->user()->foto}}" alt="" class="usuario">
                     @else   
@@ -88,8 +88,8 @@
                     @endif
                 </div>
 
-                <li class="nav-link dropdown">
-                    <a class="nav-link dropdown-toggle text-white" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="text-decoration: none"></a>
+                <li class="nav-link dropdown p-0">
+                    <a class="nav-link dropdown-toggle text-white mr-5 pt-2 mt-2" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="text-decoration: none"></a>
                         <ul class="dropdown-menu bg" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" style="" href="{{ route('inicio.contrasena') }}">
                             Cambiar Contraseña</a></li>
