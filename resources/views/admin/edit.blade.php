8f8bfc239537e00460cb42a220c4e6978c9bdc9d
Johan Morales <johan.morales@cellvoz.com.co>
2023-03-31 12:26:57 -0500

    REFACTOR: Cambio lista desplegable editar usuario

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -116,7 +116,7 @@
                         </div>
                         <div class="form-group col-4">
                             {!! Form::label('empresa_id', 'Empresa') !!}
-                            {!! Form::select('empresa_id', $empresas->pluck('nombre')
+                            {!! Form::select('empresa_id', $empresas->pluck('nombre', 'id')
                                 ,null, ['class'=>'form-control','placeholder'=>'Ingrese empresa']) !!}
                             @error('empresa_id')
                                 <span class="text-danger">{{$message }}</span>
@@ -150,18 +150,18 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-0" > 
+                        <div class="col-0" >
                             <a class="btn btn-gris" href="{{ url('admin/users') }}">Volver</a>
                         </div>
                         <div class="col-2" >
                             {!! Form::submit('Actualizar Colaborador', ['id'=>'btn2','class'=>'btn btn-magenta']) !!}
                             {!!Form::close()!!}
                         </div>
-                        <div class="col-1" > 
+                        <div class="col-1" >
                             {!!Form::model($user,['route'=> ['admin.users.restpassword',$user],'method'=>'put','id'=>'form']) !!}
                                 {!! Form::submit('Restablecer contraseña', ['id'=>'btn1','class'=>'btn btn-magenta']) !!}
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
